4fb50cfda01e3c16a279cdb820261c23351839de
Han Che <hanche2001@gmail.com>
2017-10-19 18:22:12 +0200

    init

---
+++ testclarity/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ClarityModule } from 'clarity-angular';
+
+@NgModule({
+  declarations: [
+    AppComponent
+
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    ClarityModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
